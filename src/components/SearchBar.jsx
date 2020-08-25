62bb7ebe2499646249e76816b259f1512c59a02f
Yew Kang Wei <yewyew6933@gmail.com>
2020-08-25 16:18:21 +0800

    get cities

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
+import CountryDatalist from "./CountryDatalist";
 
 export default function SearchBar() {
+  const countryEl = useRef();
+
+  async function handleCountrySearch() {
+    const selectedCountry = countryEl.current.value;
+    console.log("Selected Country:", selectedCountry);
+
+    const res = await fetch(
+      "https://parseapi.back4app.com/classes/Continentscountriescities_City?limit=4&order=cityId,country,name&keys=name,country,cityId",
+      {
+        headers: {
+          "X-Parse-Application-Id": "OnFZE2MGDk9qVwzeOxLS7oeBR9Oc0ZyrzRBGy2Vc",
+          "X-Parse-REST-API-Key": "QtabV9ysL0bRYBFQQJpnJaJmwo9S2ctaT2Z3RCva",
+        },
+      }
+    );
+    console.log(res.data);
+  }
+
   return (
     <div className="form-row">
+      {/* Search Input */}
       <div className="col-8">
         <input
-          type="text"
+          type="search"
           className="form-control"
           placeholder="Enter a country"
+          list="countries"
+          ref={countryEl}
         />
       </div>
 
-      <button className="btn btn-primary btn-md">Search</button>
+      {/* All Country Datalist */}
+      <CountryDatalist />
+
+      {/* Search Button */}
+      <button className="btn btn-primary btn-md" onClick={handleCountrySearch}>
+        Search
+      </button>
     </div>
   );
 }
