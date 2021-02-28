90903b679a7e48792c2cb75f2d9be4d4c6c515d0
Yew Kang Wei <yewyew6933@gmail.com>
2021-02-28 15:14:41 +0800

    setup display description

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -36,19 +36,9 @@ export default function SearchPage() {
           <div className="subtitle semi-bold mb-3 text-muted">
             Timezone: {placeData ? placeData.timezone : ""}
           </div>
-          {/* <p className="description text-muted">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
-            assumenda nobis maiores error, necessitatibus, maxime dolores
-            libero, quo natus nam labore neque atque dignissimos unde delectus
-            consequatur recusandae animi. Ducimus. Lorem ipsum dolor, sit amet
-            consectetur adipisicing elit. Voluptates quidem modi necessitatibus
-            molestiae totam nisi est error culpa alias eius? Lorem ipsum dolor,
-            sit amet consectetur adipisicing elit. Officia natus repellat
-            consequatur qui dolorum magni unde error eveniet pariatur ullam,
-            exercitationem eius, libero ad porro quis! Quis autem ipsam possimus
-            doloremque animi soluta, officiis consequatur laboriosam eveniet
-            itaque minus placeat.
-          </p> */}
+          {placeData?.description && (
+            <p className="description text-light">{placeData.description}</p>
+          )}
         </div>
       </div>
 
