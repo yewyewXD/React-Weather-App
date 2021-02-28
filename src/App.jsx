e3dd9fe6bc53ea6a944ea63f89f2d6df969a0922
Yew Kang Wei <yewyew6933@gmail.com>
2021-02-28 19:58:08 +0800

    refactor style structure and import google font from index html

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,6 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { GlobalProvider } from "./context/GlobalState";
 
-// styles
-import "bootstrap/dist/css/bootstrap.css";
-import "./styles/global.scss";
-import "./styles/LandingStyles.scss";
-import "./styles/SearchStyles.scss";
-
 // pages
 import SearchPage from "./pages/SearchPage";
 import LandingPage from "./pages/LandingPage";
